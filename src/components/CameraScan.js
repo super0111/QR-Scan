5da92085f5e92bfdc6882fd8424de48626d20218
chris bishop <ychrisbishop@gmail.com>
2022-07-13 03:18:00 +0900

    Update structure and design

--- src/components/CameraScan.js
+++ src/components/CameraScan.js
@@ -1,37 +1,3 @@
-// import React, { useState } from "react";
-// import QrReader from "react-qr-reader";
-
-// const CameraScan = () => {
-//   const [ state, setState ] = useState({
-//     delay: 300,
-//     result: "No result"
-//   })
-
-//   const handleScan = (data) => {
-//     if (data) {
-//       setState({
-//         result: data
-//       });
-//     }
-//   }
-//   const handleError = (err) => {
-//     console.error(err);
-//   }
-//   return (
-//     <div>
-//       <QrReader
-//         delay={state.delay}
-//         onError={handleError}
-//         onScan={handleScan}
-//         style={{ width: "100%" }}
-//       />
-//       <p>{state.result}</p>
-//     </div>
-//   );
-// }
-
-// export default CameraScan
-
 import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
 
@@ -42,11 +8,12 @@ const previewStyle = {
   border: '1px solid white !important',
 }
 
+
 const CameraScan = (props) => {
   const [data, setData] = useState('No result');
 
   return (
-    <>
+    <div className='camera-scanner'>
       <QrReader
         onResult={(result, error) => {
           if (!!result) {
@@ -63,7 +30,7 @@ const CameraScan = (props) => {
         videoId={props.videoId}
       />
       <p>{data}</p>
-    </>
+    </div>
     
   );
 };
